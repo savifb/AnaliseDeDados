ca27cbc1975766a64794e4b9fdc0e9a08f4c4c44
Savio <savio.sousa@estudante.ifb.edu.br>
2024-04-08 13:24:50 -0300

    Adicionando pasta Integracao e PANDAS

--- Integracao-txt-pdf/at1.py
+++ Integracao-txt-pdf/at1.py
@@ -1,2 +1,5 @@
-arquivo = open('/Alunos.txt', 'r')
-print(arquivo.read())
+import os
+dir_path = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(dir_path, 'Alunos.txt')
+arquivo = open(file_path, 'r')
+print(arquivo.readlines())
